2100a8124917ef1b1e678d89e300e17570a763e9
ccushma2 <ccushma2@bates.edu>
2014-05-12 19:57:30 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Politicalpal::Application.config.secret_key_base = '5bbd4212201882974cdd7af013d43137af4f227bd8e1d9da92f9445a71e5e70dfc1fbba740165ea478b6528bd98a59114e83bd0a4c6956ba3cfd4e89469b020d'
